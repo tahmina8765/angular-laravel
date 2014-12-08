63f40d27c1eb25fd8ffde5a58c5d88a323ccb26f
tahmina8765 <tahmina8765@yahoo.com>
2014-12-08 11:47:48 +0600

    Customer and Transaction model has been created

---
+++ app/models/Customer.php
@@ -0,0 +1,11 @@
+<?php
+
+class Customer extends Eloquent
+{
+
+    public function transactions()
+    {
+        return $this->hasMany('Transaction');
+    }
+
+}
---
+++ app/models/Transaction.php
@@ -0,0 +1,11 @@
+<?php
+
+class Transaction extends Eloquent
+{
+
+    public function customer()
+    {
+        return $this->belongsTo('Customer');
+    }
+
+}
